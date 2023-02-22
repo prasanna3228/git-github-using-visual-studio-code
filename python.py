e8d39566c89031cdca9070a05a30da4f410a2e03
prasanna3228 <prasannakumar3228@gmail.com>
2023-02-22 17:23:24 +0530

    done

--- python.py
+++ python.py
@@ -1,5 +1,8 @@
 print("hello world")
-if 100==100:
-  print("kumar")
-#no more changed this program
-#deleted one line
+if 100 == 100:
+    print("kumar")
+# no more changed this program
+# deleted one line
+
+
+hi prasanna kumar
